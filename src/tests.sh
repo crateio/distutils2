7209867e5462e315392acff61129e7b724fdcfed
Tarek Ziade <tarek@ziade.org>
2010-05-12 17:42:42 +0200

    fancy output

--- src/tests.sh
+++ src/tests.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+echo Testing with Python 2.4....
 python2.4 runtests.py -q
+
+echo
+echo Testing with Python 2.5....
 python2.5 runtests.py -q
+
+echo
+echo Testing with Python 2.6....
 python2.6 runtests.py -q
 
