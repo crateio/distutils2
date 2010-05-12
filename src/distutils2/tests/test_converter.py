f3b5e22965840b308395072827fc5da509235483
Tarek Ziade <tarek@ziade.org>
2010-05-12 12:39:46 +0200

    now test_conversion will try to convert files from conversions/XX_before.py and assert the result is conversions/XX_after.py. This will make it easier to add more refactoring use cases.

--- src/distutils2/tests/test_converter.py
+++ src/distutils2/tests/test_converter.py
@@ -1,5 +1,9 @@
 """Tests for distutils.converter."""
+import os
 import unittest2
+
+_CURDIR = os.path.dirname(__file__)
+
 from distutils2.converter import DistutilsRefactoringTool
 
 _ORIGINAL = """\
@@ -13,13 +17,32 @@ from distutils2.core import setup
 
 setup(name='Foo')
 """
+
+def _read_file(path):
+    # yes, distutils2 is 2.4 compatible, so, no with...
+    f = open(path)
+    try:
+        return f.read()
+    finally:
+        f.close()
+
+
 class ConverterTestCase(unittest2.TestCase):
 
-    def test_import(self):
-        # simplest case: renaming distutils import in setup.py
+
+    def test_conversions(self):
+        # for all XX_before in the conversions/ dir
+        # we run the refactoring tool
         ref = DistutilsRefactoringTool()
-        res = ref.refactor_string(_ORIGINAL, 'setup.py')
-        self.assertEquals(str(res), _WANTED)
+        convdir = os.path.join(_CURDIR, 'conversions')
+        for file_ in os.listdir(convdir):
+            if 'after' in file_ or not file_.endswith('py'):
+                continue
+            original = _read_file(os.path.join(convdir, file_))
+            wanted = file_.replace('before', 'after')
+            wanted = _read_file(os.path.join(convdir, wanted))
+            res = ref.refactor_string(original, 'setup.py')
+            self.assertEquals(str(res), wanted)
 
 def test_suite():
     return unittest2.makeSuite(ConverterTestCase)
