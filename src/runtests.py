307c0e24cbca558d564aa3eaa16d4eb8630e4263
Tarek Ziade <tarek@ziade.org>
2010-05-12 17:44:26 +0200

    make sure unittest2 is present

--- src/runtests.py
+++ src/runtests.py
@@ -3,11 +3,11 @@
 The tests for distutils2 are defined in the distutils2.tests package;
 """
 import sys
-import distutils2.tests
-from distutils2.tests import run_unittest, reap_children
-from distutils2._backport.tests import test_suite as btest_suite
 
 def test_main():
+    import distutils2.tests
+    from distutils2.tests import run_unittest, reap_children
+    from distutils2._backport.tests import test_suite as btest_suite
     # just supporting -q right now
     # to enable detailed/quiet output
     if len(sys.argv) > 1:
@@ -20,4 +20,10 @@ def test_main():
     reap_children()
 
 if __name__ == "__main__":
+    try:
+        import unittest2
+    except ImportError:
+        print('!!! You need to install unittest2')
+        sys.exit(1)
+
     test_main()
