0cdd81d2bc839e4a2a2a533150ad60c873b0dfff
Konrad Delong <konryd@gmail.com>
2010-05-13 22:02:57 +0200

    added zip_dir_into

--- src/distutils2/command/upload_docs.py
+++ src/distutils2/command/upload_docs.py
@@ -1,5 +1,18 @@
-import os.path
+import os.path, tempfile, zipfile
 from distutils2.core import Command
+
+def zip_dir_into(directory, destination):
+    """Compresses recursively contents of directory into a zipfile located
+    under given destination.
+    """
+    zip_file = zipfile.ZipFile(destination, "w")
+    for root, dirs, files in os.walk(directory):
+        for name in files:
+            full = os.path.join(root, name)
+            relative = root[len(directory):].lstrip(os.path.sep)
+            dest = os.path.join(relative, name)
+            zip_file.write(full, dest)
+    zip_file.close()
 
 class upload_docs(Command):
 
--- src/distutils2/tests/test_upload_docs.py
+++ src/distutils2/tests/test_upload_docs.py
@@ -1,30 +1,50 @@
 """Tests for distutils.command.upload_docs."""
 # -*- encoding: utf8 -*-
-import os, os.path, unittest2
+import os, os.path, tempfile, unittest2, zipfile
 
-from distutils2.command.upload_docs import upload_docs
+from distutils2.command.upload_docs import upload_docs, zip_dir_into
 from distutils2.core import Distribution
 
+from distutils2.tests import support
 from distutils2.tests.pypi_server import PyPIServer
 from distutils2.tests.test_upload import PyPIServerTestCase
 from distutils2.tests.test_config import PYPIRC, PyPIRCCommandTestCase
 
 
-class UploadDocsTestCase(PyPIServerTestCase, PyPIRCCommandTestCase):
+class UploadDocsTestCase(PyPIServerTestCase,
+                         support.TempdirManager,
+                         unittest2.TestCase):
 
     def setUp(self):
         super(UploadDocsTestCase, self).setUp()
         self.dist = Distribution()
         self.cmd = upload_docs(self.dist)
 
-    def test_generates_uploaddir_if_none(self):
+    def test_generates_uploaddir_name_not_given(self):
         self.cmd.ensure_finalized()
         self.assertEqual(self.cmd.upload_dir, os.path.join("build", "docs"))
+
+    def test_zip_dir_into(self):
+        source_dir = tempfile.mkdtemp()
+        os.mkdir(os.path.join(source_dir, "some_dir"))
+        some_file = open(os.path.join(source_dir, "some_dir", "some_file"), "w")
+        some_file.write("Ce mortel ennui")
+        some_file.close()
+        dest_dir = tempfile.mkdtemp()
+        destination = os.path.join(dest_dir, "archive.zip")
+        zip_dir_into(source_dir, destination)
+
+        self.assertTrue(zipfile.is_zipfile(destination))
+        zip_f = zipfile.ZipFile(destination)
+        self.assertEqual(zip_f.namelist(), ['some_dir/some_file'])
 
     def test_zip_dir(self):
         pass
 
     def test_upload(self):
+        pass
+
+    def test_honours_dry_run(self):
         pass
 
 def test_suite():
