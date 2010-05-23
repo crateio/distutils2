bd96b79c2d9525e86daa0748a9d89d7010ac85b6
Nicolas Cadou <ncadou@cadou.ca>
2010-05-22 21:33:38 -0400

    Changed unknown option warning to be more informative

--- src/distutils2/dist.py
+++ src/distutils2/dist.py
@@ -248,7 +248,7 @@ Common commands: (see '--help-commands' for more)
                 elif hasattr(self, key):
                     setattr(self, key, val)
                 else:
-                    msg = "Unknown distribution option: %s" % repr(key)
+                    msg = "Metadata option not defined in version 1.2 (PEP-345): %s" % repr(key)
                     if warnings is not None:
                         warnings.warn(msg)
                     else:
