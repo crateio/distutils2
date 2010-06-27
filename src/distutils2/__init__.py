ad296ff4974502ededf8fdd04fd00e0d93747877
Éric Araujo <merwok@netwok.org>
2010-06-27 02:15:52 +0200

    Remove costly shortcut in __init__ and improve the docstring

--- src/distutils2/__init__.py
+++ src/distutils2/__init__.py
@@ -1,16 +1,20 @@
 """distutils
 
-The main package for the Python Module Distribution Utilities.  Normally
-used from a setup script as
+The main package for the Python Distribution Utilities 2.  Setup
+scripts should import the setup function from distutils2.core:
 
-   from distutils.core import setup
+    from distutils2.core import setup
 
-   setup (...)
+    setup(name=..., version=..., ...)
+
+Third-party tools can use parts of Distutils2 as building blocks
+without causing the other modules to be imported:
+
+    import distutils2.version
+    import distutils2.pypi.simple
+    import distutils2.tests.pypi_server
 """
-__all__ = ['__version__', 'setup']
+__all__ = ['__version__']
 
 __revision__ = "$Id: __init__.py 78020 2010-02-06 16:37:32Z benjamin.peterson $"
 __version__ = "1.0a2"
-
-from distutils2.core import setup
-
