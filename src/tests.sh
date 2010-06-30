fb333f6cf929b27de38e04455660f8ddd58a1688
Éric Araujo <merwok@netwok.org>
2010-06-30 18:10:13 +0200

    Get more info from tests and also run with Python 2.7.
    We redirect stderr to /dev/null temporarily, when warnings are properly
    caught in Python this redirection will go away, allowing us to see real
    test errors.

--- src/tests.sh
+++ src/tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo -n "Running tests for Python 2.4... "
-python2.4 runtests.py -q > /dev/null 2> /dev/null
+python2.4 -Wd runtests.py -q 2> /dev/null
 if [ $? -ne 0 ];then
     echo "Failed"
     exit 1
@@ -9,7 +9,7 @@ else
 fi
 
 echo -n "Running tests for Python 2.5... "
-python2.5 runtests.py -q > /dev/null 2> /dev/null
+python2.5 -Wd runtests.py -q 2> /dev/null
 if [ $? -ne 0 ];then
     echo "Failed"
     exit 1
@@ -18,7 +18,7 @@ else
 fi
 
 echo -n "Running tests for Python 2.6... "
-python2.6 runtests.py -q > /dev/null 2> /dev/null
+python2.6 -Wd -bb -3 runtests.py -q 2> /dev/null
 if [ $? -ne 0 ];then
     echo "Failed"
     exit 1
@@ -26,3 +26,11 @@ else
     echo "Success"
 fi
 
+echo -n "Running tests for Python 2.7... "
+python2.7 -Wd -bb -3 runtests.py -q 2> /dev/null
+if [ $? -ne 0 ];then
+    echo "Failed"
+    exit 1
+else
+    echo "Success"
+fi
