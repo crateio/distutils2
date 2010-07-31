437132754ddf4e5c839b5de56c2973006744c14e
Éric Araujo <merwok@netwok.org>
2010-07-31 14:13:31 +0200

    Various fixes.
    - Set inplace option to build_ext in setup.cfg (this option will be
    used by all builds, not only with tests.sh)
    - Don’t build _hashlib in 2.5 (it’s in the stdlib)
    - Remove the .so on error only

--- src/tests.sh
+++ src/tests.sh
@@ -1,20 +1,18 @@
 #!/bin/sh
 echo -n "Running tests for Python 2.4... "
-rm -rf *.so
-python2.4 setup.py build_ext -i -q 2> /dev/null > /dev/null
+rm -f _hashlib.so
+python2.4 setup.py build_ext -q 2> /dev/null > /dev/null
 python2.4 -Wd runtests.py -q 2> /dev/null
-rm -rf *.so
 if [ $? -ne 0 ];then
     echo "Failed"
+    rm -f _hashlib.so
     exit 1
 else
     echo "Success"
 fi
 
 echo -n "Running tests for Python 2.5... "
-python2.5 setup.py build_ext -i -q 2> /dev/null > /dev/null
 python2.5 -Wd runtests.py -q 2> /dev/null
-rm -rf *.so
 if [ $? -ne 0 ];then
     echo "Failed"
     exit 1
