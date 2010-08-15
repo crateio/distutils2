b61bf1a96cc8934ab7b2ed6a10cce6aca400dd7d
Konrad Delong <konryd@gmail.com>
2010-08-15 20:41:05 +0200

    removed sample dists (not used in tests any more)

--- src/distutils2/tests/dists/extensions_test/setup.py
+++
@@ -1,6 +0,0 @@
-from distutils2.core import setup, Extension
-setup(name='somedist',
-      version='0.1',
-      py_modules=['myowntestmodule', 'somemod'],
-      ext_modules=[Extension('spam', ['spammodule.c'])],
-)
--- src/distutils2/tests/dists/simple_test/myowntestmodule.py
+++
@@ -1,6 +0,0 @@
-import unittest
-class SomeTest(unittest.TestCase):
-    def test_blah(self):
-        self.fail("horribly")
-def test_suite():
-    return unittest.makeSuite(SomeTest)
