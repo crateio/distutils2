7d00f4e6ffb11acddd8d753da2bbc5c364d5680b
Tarek Ziade <tarek@ziade.org>
2010-10-25 02:02:48 +0200

    cleanup

--- distutils2/__init__.py
+++ distutils2/__init__.py
@@ -1,12 +1,5 @@
 """distutils
 
-The main package for the Python Distribution Utilities 2.  Setup
-scripts should import the setup function from distutils2.core:
-
-    from distutils2.core import setup
-
-    setup(name=..., version=..., ...)
-
 Third-party tools can use parts of Distutils2 as building blocks
 without causing the other modules to be imported:
 
@@ -16,7 +9,6 @@ without causing the other modules to be imported:
 """
 __all__ = ['__version__']
 
-__revision__ = "$Id: __init__.py 78020 2010-02-06 16:37:32Z benjamin.peterson $"
 __version__ = "1.0a3"
 
 
