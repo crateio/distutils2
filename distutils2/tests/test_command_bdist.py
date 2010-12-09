183622cb8e2793b57719eb3a70a13fd25bfc298a
Yannick Gingras <ygingras@ygingras.net>
2010-12-08 22:28:40 -0500

    improved test coverage for command.bdist

--- distutils2/tests/test_command_bdist.py
+++ distutils2/tests/test_command_bdist.py
@@ -2,8 +2,9 @@
 
 from distutils2.tests import run_unittest
 
-from distutils2.command.bdist import bdist
-from distutils2.tests import unittest, support
+from distutils2.command.bdist import bdist, show_formats
+from distutils2.tests import unittest, support, captured_stdout
+
 
 class BuildTestCase(support.TempdirManager,
                     unittest.TestCase):
@@ -27,6 +28,16 @@ class BuildTestCase(support.TempdirManager,
         found = cmd.format_command.keys()
         found.sort()
         self.assertEqual(found, formats)
+
+    def test_show_formats(self):
+        __, stdout = captured_stdout(show_formats)
+
+        # the output should be a header line + one line per format
+        num_formats = len(bdist.format_commands)
+        output = [line for line in stdout.split('\n')
+                  if line.strip().startswith('--formats=')]
+        self.assertEqual(len(output), num_formats)
+
 
 def test_suite():
     return unittest.makeSuite(BuildTestCase)
