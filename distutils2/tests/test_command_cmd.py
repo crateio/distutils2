6f21bc0a8a1fadfa505e3b40c800097c60af2f76
Louis Munro <lm@louismunro.com>
2010-12-08 22:00:42 -0500

    Removed an unused method (test_ensure_string_list was defined twice).

--- distutils2/tests/test_command_cmd.py
+++ distutils2/tests/test_command_cmd.py
@@ -17,22 +17,6 @@ class CommandTestCase(unittest.TestCase):
         dist = Distribution()
         self.cmd = MyCmd(dist)
 
-    def test_ensure_string_list(self):
-
-        cmd = self.cmd
-        cmd.not_string_list = ['one', 2, 'three']
-        cmd.yes_string_list = ['one', 'two', 'three']
-        cmd.not_string_list2 = object()
-        cmd.yes_string_list2 = 'ok'
-        cmd.ensure_string_list('yes_string_list')
-        cmd.ensure_string_list('yes_string_list2')
-
-        self.assertRaises(DistutilsOptionError,
-                          cmd.ensure_string_list, 'not_string_list')
-
-        self.assertRaises(DistutilsOptionError,
-                          cmd.ensure_string_list, 'not_string_list2')
-
     def test_make_file(self):
 
         cmd = self.cmd
