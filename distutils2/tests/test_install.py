04fec684a421e232c613a5cd4fe9d13e21e28930
Yannick Gingras <ygingras@ygingras.net>
2011-01-29 20:37:55 -0500

    added some cleanup after the tests to make sure that we don't leak temp files

--- distutils2/tests/test_install.py
+++ distutils2/tests/test_install.py
@@ -39,6 +39,11 @@ class ToInstallDist(object):
             for f in range(0,3):
                self._real_files.append(mkstemp())
 
+    def _unlink_installed_files(self):
+        if self._files:
+            for f in self._real_files:
+                os.unlink(f[1])
+
     def get_installed_files(self, **args):
         if self._files:
             return [f[1] for f in self._real_files]
@@ -341,6 +346,7 @@ class TestInstall(TempdirManager, unittest.TestCase):
             for dist in remove:
                 for f in dist.get_installed_files():
                     self.assertTrue(os.path.exists(f))
+                dist._unlink_installed_files()
         finally:
             install.install_dist = old_install_dist
             install.uninstall = old_uninstall
