811d3b1ad80670854555a11b9d85475e753995ab
FELD Boris <lothiraldan@gmail.com>
2011-01-31 17:37:12 +0100

    Improve compatibility with pep8 for file datafiles.py

--- distutils2/datafiles.py
+++ distutils2/datafiles.py
@@ -1,5 +1,4 @@
 import os
-import re
 from distutils2.util import iglob
 
 __all__ = ['iglob', 'resources_dests']
@@ -17,11 +16,12 @@ class SmartGlob(object):
         else:
             base = basepath
         if '*' in base or '{' in base or '}'  in base:
-            raise NotImplementedError('glob are not supported into base part of datafiles definition. %r is an invalide basepath' % base)
+            raise NotImplementedError('glob are not supported into base part\
+                of datafiles definition. %r is an invalide basepath' % base)
         absglob = os.path.join(base, self.suffix)
-        for file in iglob(absglob):
-            path_suffix = file[len(base):].lstrip('/')
-            relpath = file[len(basepath):].lstrip('/')
+        for glob_file in iglob(absglob):
+            path_suffix = glob_file[len(base):].lstrip('/')
+            relpath = glob_file[len(basepath):].lstrip('/')
             dest = os.path.join(destination, path_suffix)
             yield relpath, dest
 
@@ -35,9 +35,9 @@ def resources_dests(resources_dir, rules):
         else:
             delete = False
             dest = glob_dest
-        for file, file_dest in sglob.expand(resources_dir, dest):
-            if delete and file in destinations:
-                del destinations[file]
+        for resource_file, file_dest in sglob.expand(resources_dir, dest):
+            if delete and resource_file in destinations:
+                del destinations[resource_file]
             else:
-                destinations[file] = file_dest
+                destinations[resource_file] = file_dest
     return destinations
