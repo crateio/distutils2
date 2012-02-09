c162453fba2e3cb6a6f413528c28290474791aa0
Éric Araujo <merwok@netwok.org>
2012-02-09 14:42:02 +0100

    Group commands by topic in “pysetup run --list-commands” output.
    This fixes a regression from distutils, where “setup.py --help-commands”
    prints out commands grouped by topic (i.e. building vs. installing),
    which is more useful than using sorted.

--- distutils2/command/__init__.py
+++ distutils2/command/__init__.py
@@ -6,38 +6,28 @@ from distutils2.util import resolve_name
 __all__ = ['get_command_names', 'set_command', 'get_command_class',
            'STANDARD_COMMANDS']
 
-_COMMANDS = {
-    'check': 'distutils2.command.check.check',
-    'test': 'distutils2.command.test.test',
-    'build': 'distutils2.command.build.build',
-    'build_py': 'distutils2.command.build_py.build_py',
-    'build_ext': 'distutils2.command.build_ext.build_ext',
-    'build_clib': 'distutils2.command.build_clib.build_clib',
-    'build_scripts': 'distutils2.command.build_scripts.build_scripts',
-    'clean': 'distutils2.command.clean.clean',
-    'install_dist': 'distutils2.command.install_dist.install_dist',
-    'install_lib': 'distutils2.command.install_lib.install_lib',
-    'install_headers': 'distutils2.command.install_headers.install_headers',
-    'install_scripts': 'distutils2.command.install_scripts.install_scripts',
-    'install_data': 'distutils2.command.install_data.install_data',
-    'install_distinfo':
-        'distutils2.command.install_distinfo.install_distinfo',
-    'sdist': 'distutils2.command.sdist.sdist',
-    'bdist': 'distutils2.command.bdist.bdist',
-    'bdist_dumb': 'distutils2.command.bdist_dumb.bdist_dumb',
-    'bdist_wininst': 'distutils2.command.bdist_wininst.bdist_wininst',
-    'register': 'distutils2.command.register.register',
-    'upload': 'distutils2.command.upload.upload',
-    'upload_docs': 'distutils2.command.upload_docs.upload_docs',
-}
 
-# XXX this is crappy
+STANDARD_COMMANDS = [
+    # packaging
+    'check', 'test',
+    # building
+    'build', 'build_py', 'build_ext', 'build_clib', 'build_scripts', 'clean',
+    # installing
+    'install_dist', 'install_lib', 'install_headers', 'install_scripts',
+    'install_data', 'install_distinfo',
+    # distributing
+    'sdist', 'bdist', 'bdist_dumb', 'bdist_wininst',
+    'register', 'upload', 'upload_docs',
+    ]
+
 if os.name == 'nt':
-    _COMMANDS['bdist_msi'] = 'distutils2.command.bdist_msi.bdist_msi'
+    STANDARD_COMMANDS.insert(STANDARD_COMMANDS.index('bdist_wininst'),
+                             'bdist_msi')
 
-# XXX use OrderedDict to preserve the grouping (build-related, install-related,
-# distribution-related)
-STANDARD_COMMANDS = set(_COMMANDS)
+# XXX maybe we need more than one registry, so that --list-comands can display
+# standard, custom and overriden standard commands differently
+_COMMANDS = dict((name, 'distutils2.command.%s.%s' % (name, name))
+                 for name in STANDARD_COMMANDS)
 
 
 def get_command_names():
