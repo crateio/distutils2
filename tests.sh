9c68f407cfe642942e6a1e1fb17e8e66c9787cca
Tarek Ziade <tarek@ziade.org>
2012-03-12 14:19:36 -0700

    removed 2.4 and 2.5

--- tests.sh
+++ tests.sh
@@ -1,25 +1,4 @@
 #!/bin/sh
-echo -n "Running tests with Python 2.4... "
-python2.4 setup.py build_ext -f -q 2> /dev/null > /dev/null
-python2.4 -Wd runtests.py -q
-if [ $? -ne 0 ];then
-    echo Failed, re-running
-    python2.4 -Wd runtests.py
-    exit $?
-else
-    echo Success
-fi
-
-echo -n "Running tests with Python 2.5... "
-python2.5 -Wd runtests.py -q
-if [ $? -ne 0 ];then
-    echo Failed, re-running
-    python2.5 -Wd runtests.py
-    exit $?
-else
-    echo Success
-fi
-
 echo -n "Running tests with Python 2.6... "
 python2.6 -Wd runtests.py -q
 if [ $? -ne 0 ];then
